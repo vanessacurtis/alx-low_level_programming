107fff4b08d8aa528c16c8bd8fcef2b4ad234165
vanessacurtis <nakalemavanessa10@gmail.com>
2023-02-17 05:27:16 -0800

    printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
 /**
  * main -  you sturbon
- * Return:  0 (Success)
+ * Return:  always 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float x;
-
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int : %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+        printf("Size of a int: %d byte(s)\n", (int) sizeof(int));
+        printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+        printf("Size of a long long: %d byte(s)\n", (int) sizeof(long long int));
+        printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 	return (0);
 }
