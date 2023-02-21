374eb2536430e941bff408d427aa9d3d5006edc1
vanessacurtis <nakalemavanessa10@gmail.com>
2023-02-21 14:10:28 -0800

    printing put char

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - A prrogram that prints _putchar
+ * and a nwe line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 95; i < = 112;  )
+	{
+		putchar(i);
+		i = i + 17;
+		}
+	for (int i = 117; i > = 116; i--)
+	{
+
+		putchar(i);
+		}
+	for (int i = 99; i < = 104; )
+	{
+		putchar(i);
+		i = i + 5;
+	}
+	for (int i = 97; i < = 114; )
+	{
+		putchar(1);
+		i = i + 17;
+	}
+	putchar('\n');
+	return (0);
+}
+
