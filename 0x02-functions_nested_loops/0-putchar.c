197f0241e5b6782dd317110fd8898238eb11a8c5
vanessacurtis <nakalemavanessa10@gmail.com>
2023-02-22 00:39:55 -0800

    Putchar program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - A prrogram that prints _putchar
- * and a nwe line
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
-	for (int i = 95; i < = 112;  )
-	{
-		putchar(i);
-		i = i + 17;
-		}
-	for (int i = 117; i > = 116; i--)
-	{
-
-		putchar(i);
-		}
-	for (int i = 99; i < = 104; )
-	{
-		putchar(i);
-		i = i + 5;
-	}
-	for (int i = 97; i < = 114; )
-	{
-		putchar(1);
-		i = i + 17;
-	}
-	putchar('\n');
-	return (0);
+        char str[] = "_putchar";
+        int ch;
+        for (ch = 0; ch < 8; ++ch)
+                _putchar(str[ch]);
+        _putchar('\n');
+        return (0);
 }
-
