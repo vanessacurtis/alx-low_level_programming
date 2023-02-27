df4a5d8ddb675a3eef03676df4d2c9841dbdb22a
vanessacurtis <nakalemavanessa10@gmail.com>
2023-02-27 05:32:08 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints out a string to stdout
+ * @str: pointer to the string to print
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; str[] != '\0'; i++)
+	{
+		_putchar("i");
+	}
+	_putchar('\n');
+}
