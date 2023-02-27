4c4d6856ad9257ba2346f2198117e5684afda3ad
vanessacurtis <nakalemavanessa10@gmail.com>
2023-02-27 03:58:25 -0800

    this reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer to an iint
+ *updates the value it points to to 98
+ *@n:pointer to update
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
