4699a5dad611482cbf18a85d881ceda986d3d92c
vanessacurtis <nakalemavanessa10@gmail.com>
2023-03-11 00:29:38 -0800

    puts

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,12 +1,7 @@
 #include "main.h"
 
-/**
- * main - prints the phrase "At the end of the day, my goal was to be the best hacker"
- * Return: 0 if exited properly, non-zero otherwise
- */
-
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
