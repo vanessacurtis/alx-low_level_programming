0a145e028ddc19b46d1e93f63dbb4591250a6e5d
vanessacurtis <nakalemavanessa10@gmail.com>
2023-03-21 07:25:48 -0700

    a macro that is SIZE

---
+++ 0x02-functions_nested_loops/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * sum- simple function tht adds two numbers
+ * Return: Always 0 Succes
+*/
+int vanessa(int x,int y){
+
+return x+y;
+}
+int main(void)
+{
+	printf("%d", vanessa(5,4)); 
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/test0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * putchar- prints the alphabet in lowercase
+ * then goes to a new line
+ */
+
+void print_alphabet(void){
+	char c = 'a';
+	while(c <='z')
+	{
+		c++;
+		_putchar('c');
+	}
+	_putchar('\n');
+
+}
+
+
