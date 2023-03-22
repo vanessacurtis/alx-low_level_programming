909e60dee9783eb65ba43d3dc54513bc88ccaeb9
vanessacurtis <nakalemavanessa10@gmail.com>
2023-03-22 07:27:00 -0700

    well defined header dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,31 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
- * struct dog - struct dog contains details about dog
- * @name: first element
- * @age: second element
- * @owner: third element
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
+ * Description: Longer description
  */
-struct dog 
+struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
